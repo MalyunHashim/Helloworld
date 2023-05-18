03afd043a85bdbe73fc53a4395f09302151347be
malyumohamed <malyunmohaz942@gmail.com>
2023-05-18 17:42:51 +0300

    added some print statement

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("am programmer");
+        int x=8;
+        int y=9;
+        System.out.println(x+y);
     }
 }
